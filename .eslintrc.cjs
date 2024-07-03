2d3724381c2b48ab6440fd2282617d2cdd80a4e7
Lucas Cassimiro <lucascassimiro545@hotmail.com>
2024-07-02 21:08:44 -0300

    chore: novas configurações

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -15,6 +15,8 @@ module.exports = {
     'react/jsx-no-target-blank': 'off',
     'react-refresh/only-export-components': 'off',
     'react-refresh/only-export-components': 'off',
+    'react-refresh/only-export-components': 'off',
+    'react/prop-types': 'off',
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
